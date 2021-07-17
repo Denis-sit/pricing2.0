024e6ed232d0fea30f39759b2678d4b141fadb37
Denis-sit <specden990@mail.ru>
2021-07-17 20:18:27 +0500

    добавлено событие resize

--- js/script.js
+++ js/script.js
@@ -1,28 +1,60 @@
 window.onload = () =>{
-   
+    const pc = document.querySelector('.pc-version'),
+    mb = document.querySelector('.mobile-version'),
+    button = pc.querySelectorAll('.advice-block__nav-button'),
+    buttonMb = mb.querySelectorAll('.advice-block__nav-button'),
+    element = pc.querySelectorAll('.advice-block__wrapper-button'),
+    parentBlock = pc.querySelector('.advice-block__nav'),
+    parentBlockMb = mb.querySelector('.advice-block__nav'),
+    elem = document.getElementById('slider'),
+    elemMb = document.getElementById('slider-mb'),
+    sliderTitle = pc.querySelector('.advice-block__text_question'),
+    sliderTitleMb = mb.querySelector('.advice-block__text_question');
+    let arrOffset = [ 110, 430, 730, 1100, 1700, 3800],
+    arrTitle = ['Как стать популярным работодателем?',
+              'Как получать больше откликов?',
+              'Как улучшить процесс отбора?',
+              'Как снизить текучесть на испытательном сроке?',
+              'Как сделать так, чтобы сотрудники работали долго и счастливо?',
+              'Как сделать так, чтобы сотрудники не уходили?'];
+    let  offset = parseInt(window.getComputedStyle(elem).getPropertyValue('left'));
+    offsetMb = parseInt(window.getComputedStyle(elemMb).getPropertyValue('left'));
+    let newOffset,
+        newOffsetMb;
+    
+    window.onresize = function(event){
+        for (let i = 0; i < button.length; i++) {
+            let btn = button[i];
+
+            if(btn.classList.contains('show')){
+                elem.style.left = 50 + '%';
+                setTimeout(() => {
+                    newOffset = parseInt(window.getComputedStyle(elem).getPropertyValue('left'));
+                    console.log('newOffset', newOffset)
+                    console.log('arrOffset[i]', arrOffset[i])
+                    console.log('b if', newOffset - arrOffset[i] + 'px');
+                    elem.style.left = newOffset - arrOffset[i] + 'px';
+                }, 1000);
+            };
+        };
+    };
+
+    window.onresize = function(event){
+        for (let i = 0; i < buttonMb.length; i++) {
+            let btn = buttonMb[i];
+
+            if(btn.classList.contains('show')){
+                elemMb.style.left = 50 + '%';
+                setTimeout(() => {
+                    newOffsetMb = parseInt(window.getComputedStyle(elemMb).getPropertyValue('left'));
+                    elemMb.style.left =  newOffsetMb - arrOffset[i] + 'px';
+                }, 1000);
+            };
+        };
+    };
+    
+
     function clickButton(){
-        const pc = document.querySelector('.pc-version'),
-              mb = document.querySelector('.mobile-version'),
-              button = pc.querySelectorAll('.advice-block__nav-button'),
-              buttonMb = mb.querySelectorAll('.advice-block__nav-button'),
-              element = pc.querySelectorAll('.advice-block__wrapper-button'),
-              parentBlock = pc.querySelector('.advice-block__nav'),
-              parentBlockMb = mb.querySelector('.advice-block__nav'),
-              elem = document.getElementById('slider'),
-              elemMb = document.getElementById('slider-mb'),
-              sliderTitle = pc.querySelector('.advice-block__text_question'),
-              sliderTitleMb = mb.querySelector('.advice-block__text_question');
-        let arrOffset = [ 110, 430, 730, 1100, 1700, 3800],
-            arrTitle = ['Как стать популярным работодателем?',
-                        'Как получать больше откликов?',
-                        'Как улучшить процесс отбора?',
-                        'Как снизить текучесть на испытательном сроке?',
-                        'Как сделать так, чтобы сотрудники работали долго и счастливо?',
-                        'Как сделать так, чтобы сотрудники не уходили?'],
-            offset = parseInt(window.getComputedStyle(elem).getPropertyValue('left'));
-            offsetMb = parseInt(window.getComputedStyle(elemMb).getPropertyValue('left'));
-              
-        
         function activeButtonByDefault(){
             button.forEach((btns, i) =>{
                 if(btns.classList.contains('show')){
@@ -77,11 +109,13 @@ window.onload = () =>{
                 });
             }
         });
+
+       
     };
     clickButton();
 
     function displayingTheSectionStatus(){     
-        const status= document.querySelectorAll('[data-on]');
+        const status = document.querySelectorAll('[data-on]');
         
         status.forEach(item =>{
             item.classList.add('status-on');
